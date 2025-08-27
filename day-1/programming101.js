792ccfca66f9acf1e95967fdf757146777a8c18d
gourav017 <gourav6471sahu@gmail.com>
2025-08-27 21:09:56 +0530

    programming101->basic maths operations

---
+++ day-1/programming101.js
@@ -0,0 +1,16 @@
+console.log("hello words")
+console.log("hi namaste javascript")
+console.log("akshay saini")
+console.log("hi i am from the namaste dsa")
+
+const a = 20;
+
+const b = 90;
+
+let sum = a + b;
+let multiple = a * b;
+let division = a - b;
+
+console.log(sum)
+console.log(multiple);
+console.log(division)
