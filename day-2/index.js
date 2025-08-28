3907fbaf67cd40ef5ede72fc4b72c30a85c5d99f
gourav017 <gourav6471sahu@gmail.com>
2025-08-28 15:45:11 +0530

    logic for 18+ vote

--- day-2/index.js
+++ day-2/index.js
@@ -24,3 +24,47 @@ doingMathsOperations(10, 4)
 // <-------------------------------------------------------------------------------------->
 
 
+function doingMathsOperations(a, b) {
+    let sum = a + b;
+    let multiple = a * b;
+    let division = a - b;
+
+    let obj = {
+        sum, multiple, division
+    }
+    return obj
+}
+let values = doingMathsOperations(10, 4)
+console.log(values)
+
+// <----------------------------------------------------------------------------------------------------->
+
+// write a function which is accept a age as a argument and check that person is eligible for vote or not
+
+typeof "John"         // Returns string
+typeof ("John" + "Doe") // Returns string
+typeof 3.14           // Returns number
+typeof 33             // Returns number
+typeof (33 + 66)      // Returns number
+typeof true           // Returns boolean
+typeof false          // Returns boolean
+typeof 1234n          // Returns bigint
+typeof Symbol()       // Returns symbol
+typeof x              // Returns undefined
+
+function eligibileToVote(age) {
+    if (typeof (age) !== "number") {
+        console.log("invalid input")
+    }
+    else if (age < 0) {
+        console.log("invalid input")
+    }
+    else if(age>=18 && age < 120) {
+        console.log("eligible for Vote")
+    }
+    else {
+        console.log("not eligible for vote")
+    }
+}
+
+eligibileToVote(9)
