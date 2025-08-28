e067611bbe669738320dace155012892516e1ed6
gourav017 <gourav6471sahu@gmail.com>
2025-08-28 15:18:58 +0530

    about function

---
+++ day-2/index.js
@@ -0,0 +1,26 @@
+function printHellowWorld() {
+ console.log("hellow world")   
+}
+
+printHellowWorld()
+
+// <------------------------------------------------------------------->
+
+function doingMathsOperations(a,b) {
+
+    let sum = a + b;
+    let multiple = a * b;
+    let division = a - b;
+
+    console.log(sum);
+    console.log(multiple);
+    console.log(division)
+
+}
+
+doingMathsOperations(10, 4)
+
+
+// <-------------------------------------------------------------------------------------->
+
+
---
+++ day-2/notes.md
@@ -0,0 +1,2 @@
+# what is function - how to write a functin play around the functions.
+so function is a programe which is return a output kind of a programming
