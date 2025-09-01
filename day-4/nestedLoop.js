9cf0fdaafef5109a72d5b8e0f009528ce01b5de4
gourav017 <gourav6471sahu@gmail.com>
2025-09-01 21:31:06 +0530

    nested loop 01

--- day-4/nestedLoop.js
+++ day-4/nestedLoop.js
@@ -1,5 +1,16 @@
+// <---------------do a dry run--------------------->
+
 for (let i = 0; i < 5; i++){
     for (let j = 0; j <= i; j++){
         console.log(i,j)
     }
 }
+
+// <---------------do a dry run--------------------->
+for (let i = 0; i < 3; i++) {
+    for (let j = i; j > 0; j--) {
+        console.log(i, j)
+    }
+}
+
+// <---------------do a dry run--------------------->
